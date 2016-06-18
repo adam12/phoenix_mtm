3ab1f0bc065c10ff5c5b803620489f69bfd4f96e
Adam Daniels <adam@mediadrive.ca>
2016-06-18 11:57:16 -0400

    Add ex_doc dependency

--- mix.exs
+++ mix.exs
@@ -21,7 +21,8 @@ defmodule Phoenix.MTM.Mixfile do
 
   defp deps do
     [{:phoenix_html, "~> 2.0"},
-      {:ecto, "~> 2.0.0-rc"}]
+     {:ecto, "~> 2.0.0-rc"},
+     {:ex_doc, ">= 0.0.0", only: :dev}]
   end
 
   defp description do
