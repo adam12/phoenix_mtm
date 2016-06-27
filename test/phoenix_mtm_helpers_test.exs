0b9f00f899ca73b7d025f61e78aa00561bee125e
Adam Daniels <adam@mediadrive.ca>
2016-06-27 13:06:25 -0400

    Add tests for helpers

---
+++ test/phoenix_mtm_helpers_test.exs
@@ -0,0 +1,30 @@
+defmodule PhoenixMTM.HelpersTest do
+  use ExUnit.Case
+  import Phoenix.HTML, only: [safe_to_string: 1]
+  import Phoenix.HTML.Form, only: [form_for: 4]
+  import PhoenixMTM.Helpers, only: [collection_checkboxes: 3]
+
+  doctest PhoenixMTM.Helpers
+
+  defp conn do
+    Plug.Test.conn(:get, "/foo", %{})
+  end
+
+  test "generates list of checkboxes and inputs" do
+    form = safe_to_string(form_for conn(), "/", [as: :form], fn f ->
+      collection_checkboxes(f, :collection, ["1": 1, "2": 2])
+    end)
+
+    assert form =~
+      ~s(<input id=\"form_collection_1\" name=\"form[collection][]\" type=\"checkbox\" value=\"1\"><label for=\"form_collection_1\">1</label><input id=\"form_collection_2\" name=\"form[collection][]\" type=\"checkbox\" value=\"2\"><label for=\"form_collection_2\">2</label>)
+  end
+
+  test "generates hidden input" do
+    form = safe_to_string(form_for conn(), "/", [as: :form], fn f ->
+      collection_checkboxes(f, :collection, ["1": 1, "2": 2])
+    end)
+
+    assert form =~
+      ~s(<input id=\"form_collection\" name=\"form[collection][]\" type=\"hidden\" value=\"\">)
+  end
+end
--- test/phoenix_mtm_test.exs
+++ test/phoenix_mtm_test.exs
@@ -1,8 +1,4 @@
 defmodule PhoenixMTMTest do
   use ExUnit.Case
   doctest PhoenixMTM
-
-  test "the truth" do
-    assert 1 + 1 == 2
-  end
 end
