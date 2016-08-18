9dbc7160b571879f9d8c5f2c94cd304f9a59ac16
Justin B Kay <jkay@nwrecc.org>
2016-08-18 14:06:22 -0600

    Refactor, add tests, improve docs
    - Refactor helper
    - Add tests to cover options
    - Demonstrate usage of input_opts and label_opts. ie. adding CSS classes

--- test/phoenix_mtm_helpers_test.exs
+++ test/phoenix_mtm_helpers_test.exs
@@ -2,7 +2,7 @@ defmodule PhoenixMTM.HelpersTest do
   use ExUnit.Case
   import Phoenix.HTML, only: [safe_to_string: 1]
   import Phoenix.HTML.Form, only: [form_for: 4]
-  import PhoenixMTM.Helpers, only: [collection_checkboxes: 3]
+  import PhoenixMTM.Helpers, only: [collection_checkboxes: 4, collection_checkboxes: 3]
 
   doctest PhoenixMTM.Helpers
 
@@ -17,6 +17,24 @@ defmodule PhoenixMTM.HelpersTest do
 
     assert form =~
       ~s(<input id=\"form_collection_1\" name=\"form[collection][]\" type=\"checkbox\" value=\"1\"><label for=\"form_collection_1\">1</label><input id=\"form_collection_2\" name=\"form[collection][]\" type=\"checkbox\" value=\"2\"><label for=\"form_collection_2\">2</label>)
+  end
+
+  test "generates list of checkboxes and inputs with a class" do
+    form = safe_to_string(form_for conn(), "/", [as: :form], fn f ->
+      collection_checkboxes(f, :collection, ["1": 1, "2": 2], input_opts: [class: "form-field"])
+    end)
+
+    assert form =~
+      ~s(<input class=\"form-field\" id=\"form_collection_1\" name=\"form[collection][]\" type=\"checkbox\" value=\"1\"><label for=\"form_collection_1\">1</label><input class=\"form-field\" id=\"form_collection_2\" name=\"form[collection][]\" type=\"checkbox\" value=\"2\"><label for=\"form_collection_2\">2</label>)
+  end
+
+  test "generates list of checkboxes and inputs with one selected element" do
+    form = safe_to_string(form_for conn(), "/", [as: :form], fn f ->
+      collection_checkboxes(f, :collection, ["1": 1, "2": 2], selected: [1])
+    end)
+
+    assert form =~
+      ~s(<input checked=\"checked\" id=\"form_collection_1\" name=\"form[collection][]\" type=\"checkbox\" value=\"1\"><label for=\"form_collection_1\">1</label><input id=\"form_collection_2\" name=\"form[collection][]\" type=\"checkbox\" value=\"2\"><label for=\"form_collection_2\">2</label>)
   end
 
   test "generates hidden input" do
