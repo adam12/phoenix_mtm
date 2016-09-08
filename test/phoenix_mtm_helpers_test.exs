6b847c591a62af1e791499dba522dff66ae35825
Jason Draper <jason@drapergeek.com>
2016-09-08 13:45:36 -0400

    Allow nesting checkboxes inside labels
    Add a helper to make the tests easier to read.

--- test/phoenix_mtm_helpers_test.exs
+++ test/phoenix_mtm_helpers_test.exs
@@ -10,6 +10,26 @@ defmodule PhoenixMTM.HelpersTest do
     Plug.Test.conn(:get, "/foo", %{})
   end
 
+  describe "when passed the :nested option" do
+    test "generates list of labels with a checkbox nested in each" do
+      form = safe_to_string(form_for conn(), "/", [as: :form], fn f ->
+        collection_checkboxes(f, :collection, ["1": 1, "2": 2], nested: true)
+      end)
+
+      assert form =~
+        ~s(
+          <label for=\"form_collection_1\">
+            1
+            <input id=\"form_collection_1\" name=\"form[collection][]\" type=\"checkbox\" value=\"1\">
+          </label>
+          <label for=\"form_collection_2\">
+            2
+            <input id=\"form_collection_2\" name=\"form[collection][]\" type=\"checkbox\" value=\"2\">
+          </label>
+        ) |> remove_outside_whitespace
+    end
+  end
+
   test "generates list of checkboxes and inputs" do
     form = safe_to_string(form_for conn(), "/", [as: :form], fn f ->
       collection_checkboxes(f, :collection, ["1": 1, "2": 2])
@@ -44,5 +64,10 @@ defmodule PhoenixMTM.HelpersTest do
 
     assert form =~
       ~s(<input id=\"form_collection\" name=\"form[collection][]\" type=\"hidden\" value=\"\">)
+  end
+
+  def remove_outside_whitespace(string) do
+    whitespace_not_inside_html_tag = ~r/\s(?=[^>]*(<|$))/
+    String.replace(string, whitespace_not_inside_html_tag, "")
   end
 end
